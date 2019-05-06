11b3e1f0843eca8b7340b21253c8ea2b93c01f83
Ramadhani Samudra Gawang Indiyanto <ganar.gatul@gmail.com>
2019-05-06 12:22:50 +0700

    pagestatefull

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: PageStateLess(),
+      home: PageStateFull(),debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -34,6 +34,43 @@ class PageStateLess extends StatelessWidget {
         child: new Container(
           color: Colors.lightGreen,
           margin:  const EdgeInsets.all(50.0),
+        ),
+      ),
+    );
+  }
+}
+
+class PageStateFull extends StatefulWidget {
+  @override
+  _PageStateFullState createState() => _PageStateFullState();
+}
+
+class _PageStateFullState extends State<PageStateFull> {
+
+  String teks='';
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    teks='Klik Tombol Ini';
+  }
+
+  void methodChange(){
+    setState(() {
+      teks ='Sudah Di Klik';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Center(
+        child: new MaterialButton(
+          
+          color: Colors.green,
+          textColor: Colors.white,
+          onPressed:(){methodChange();} ,
+          child: Text(teks)
         ),
       ),
     );
