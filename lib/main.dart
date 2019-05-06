a853383df30b8fd876bfed318f176483454e0a5c
Ramadhani Samudra Gawang Indiyanto <ganar.gatul@gmail.com>
2019-05-06 21:16:01 +0700

    row and coloumn

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: PageWidgetContainer(),debugShowCheckedModeBanner: false,
+      home: PageRowColumn(),debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -134,6 +134,29 @@ class PageWidgetContainer extends StatelessWidget {
         foregroundDecoration: new BoxDecoration(
           color: Colors.lightGreen
         ),
+      ),
+    );
+  }
+}
+
+class PageRowColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("This"),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("This"),
+              Text("Is"),
+              Text("Coloumn"),
+            ],
+          ),
+          Text("Row")
+        ],
       ),
     );
   }
