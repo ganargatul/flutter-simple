341ddbff4de90b7522beefdc7d5ebb415cd6734e
Ramadhani Samudra Gawang Indiyanto <ganar.gatul@gmail.com>
2019-05-08 23:12:17 +0700

    input + set state

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: PageTabBar(),debugShowCheckedModeBanner: false,
+      home: PageInputWidget(),debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -304,6 +304,50 @@ class _PageTabBarState extends State<PageTabBar> with SingleTickerProviderStateM
           Center(
             child: Text("Welcome To Data Usage"),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class PageInputWidget extends StatefulWidget {
+  @override
+  _PageInputWidgetState createState() => _PageInputWidgetState();
+}
+
+class _PageInputWidgetState extends State<PageInputWidget> {
+  
+  String txt = "";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("Page Input"),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            onChanged: (String text){
+              setState(() {
+                txt = text;
+              });
+            },
+            decoration: InputDecoration(
+              hintText: "Input Username",
+              labelText: "Username"
+            ),
+          ),
+          //kie nggo margin
+          SizedBox(height: 10,),
+          //kie jiot txt
+          Text(txt)
         ],
       ),
     );
